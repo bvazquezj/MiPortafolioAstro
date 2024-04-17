dccff3e4ec4350e9d8b180c2adff07a8562948fd
Brandon Vázquez <bvazquezj1700@gmail.com>
2024-04-16 23:45:39 -0600

    Agregando componentes y Tailwind

---
+++ src/components/test.js
@@ -0,0 +1,10 @@
+const PROYECTS = [
+    {
+        title: 'proyecto 1',
+        description: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+        image: './images/proyecto1.jpg',
+        link: '#',
+        github: '#',
+        tech: ['html', 'css', 'javascript']
+    }
+]
